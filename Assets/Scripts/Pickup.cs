f7041828afe9d54961f3030c89a0077d87128a2e
hahaghostt <kbinas2022@elam.co.uk>
2022-11-22 12:43:24 +0000

    press e

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public bool isinside = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        isinside = true;
+        //if (Input.GetKeyDown(KeyCode.E))
+        //Destroy (game.Object); 
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isinside = false;
+        //if (Input.GetKeyDown(KeyCode.E))
+        //Destroy (game.Object); 
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isinside == true)
+        {
+
+            Destroy(gameObject);
+
+        }
+    }
+}
